b751ad5f89ccf43136d2a0c7598f0737bdac08c3
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2023-12-04 00:10:30 +0700

    Version 1.0.0a

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var query = document.getElementById('searchInput').value;
+    search(query);
+});
+
+function search(query) {
+    // Perform search logic here
+    // You can use AJAX to fetch search results from a server or perform client-side filtering
+    // Display the search results in the 'searchResults' div
+    var searchResults = document.getElementById('searchResults');
+    if(query == 'flowerful' || query == 'Flowerful' || query == 'FLOWERFUL'){
+        var result = 'Diadakan pada tanggal 17 Desember 2023 di Graha Unesa Surabaya';}
+    else if(query == 'vc' || query == 'video call' || query == 'VC' || query == 'videocall'){
+        var result = 'Fans JKT48 yang terhormat, kami mohon maaf atas ketidaknyamanannya. Sehubungan dengan masih terdapatnya pembatasan akses masuk ke situs web JKT48, imbas tingginya antusiasme fans JKT48 yang ingin membeli tiket VC. Pembatasan akses masuk ke situs web tercinta akan dilakukan sampai waktu yang belum dapat ditentukan. Terima kasih.';
+    }
+    else if(query == 'live streaming' || query == 'streaming' || query == 'live stream' || query == 'stream'){
+        var result = 'Fans JKT48 yang terhormat, kami mohon maaf atas ketidaknyamanannya. Sehubungan dengan masih terdapatnya perbaikan serta pengecekan baik pada jaringan maupun perangkat lunak, maka live streaming ditiadakan untuk sementara waktu. Jika live streaming dilanjutkan, informasi tersebut akan disampaikan sesegera mungkin setelah perbaikan selesai. Terima kasih.';
+    }
+    else{
+        var result = 'Maaf, kami tidak dapat menemukan hasil pencarian untuk: ' + query + '. Silahkan coba lagi.';
+    }
+    searchResults.innerHTML = 'Hasil pencarian untuk: ' + query + "<br />" + result;
+}
