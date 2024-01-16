50f59c59521196b9fbfe785f829f73a16451d3d5
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-01-16 20:52:31 +0700

    fix bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ function search(query) {
   // You can use AJAX to fetch search results from a server or perform client-side filtering
   // Display the search results in the 'searchResults' div
   var searchResults = document.getElementById("searchResults");
+  var searchContents = document.getElementById("searchContents");
   if (query == "flowerful" || query == "Flowerful" || query == "FLOWERFUL") {
     var result =
       "Diadakan pada tanggal 17 Desember 2023 di Graha Unesa Surabaya";
@@ -26,7 +27,7 @@ function search(query) {
     query == "VC" ||
     query == "videocall"
   ) {
-    var result =
+    result =
       "Fans JKT48 yang terhormat, kami mohon maaf atas ketidaknyamanannya. Sehubungan dengan masih terdapatnya pembatasan akses masuk ke situs web JKT48, imbas tingginya antusiasme fans JKT48 yang ingin membeli tiket VC. Pembatasan akses masuk ke situs web tercinta akan dilakukan sampai waktu yang belum dapat ditentukan. Terima kasih.";
   } else if (
     query == "live streaming" ||
@@ -34,14 +35,16 @@ function search(query) {
     query == "live stream" ||
     query == "stream"
   ) {
-    var result =
+    result =
       "Fans JKT48 yang terhormat, kami mohon maaf atas ketidaknyamanannya. Sehubungan dengan masih terdapatnya perbaikan serta pengecekan baik pada jaringan maupun perangkat lunak, maka live streaming ditiadakan untuk sementara waktu. Jika live streaming dilanjutkan, informasi tersebut akan disampaikan sesegera mungkin setelah perbaikan selesai. Terima kasih.";
   } else {
-    var result =
+    result =
       "Maaf, kami tidak dapat menemukan hasil pencarian untuk: " +
       query +
       ". Silahkan coba lagi.";
   }
-  searchResults.innerHTML =
-    "Hasil pencarian untuk: " + query + "<br />" + result;
+  searchContents.textContent =
+    "Hasil pencarian untuk: " + query;
+  searchResults.textContent =  
+    result;
 }
